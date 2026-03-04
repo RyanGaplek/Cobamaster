22972e45a742c716aa7a421eb2c4982cd47b5f3d
RyanGaplek <92612190+RyanGaplek@users.noreply.github.com>
2026-03-04 12:13:32 +0700

    Add files via upload

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    { name: "Bot-1", script: "bot1.py", interpreter: "python3" },
+    { name: "Bot-2", script: "bot2.py", interpreter: "python3" },
+    { name: "Bot-3", script: "bot3.py", interpreter: "python3" },
+    { name: "Bot-4", script: "bot4.py", interpreter: "python3" },
+    { name: "Bot-5", script: "bot5.py", interpreter: "python3" },
+    { name: "Bot-6", script: "bot6.py", interpreter: "python3" },
+    { name: "Bot-7", script: "bot7.py", interpreter: "python3" },
+    { name: "Bot-8", script: "bot8.py", interpreter: "python3" },
+    { name: "Bot-9", script: "bot9.py", interpreter: "python3" },
+    { name: "Bot-10", script: "bot10.py", interpreter: "python3" },
+    { name: "Bot-11", script: "bot11.py", interpreter: "python3" },
+    { name: "Bot-12", script: "bot12.py", interpreter: "python3" },
+    { name: "Bot-13", script: "bot13.py", interpreter: "python3" },
+    { name: "Bot-14", script: "bot14.py", interpreter: "python3" },
+    { name: "Bot-15", script: "bot15.py", interpreter: "python3" }
+  ]
+};
